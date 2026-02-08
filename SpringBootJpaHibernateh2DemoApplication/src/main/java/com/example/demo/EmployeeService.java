4b13fa7eeec274948edef3a4f7ba3f6198640d9b
DivyaVoruganti <divya.variganti@gmail.com>
2026-02-08 23:35:24 +0530

    spring practice

---
+++ SpringBootJpaHibernateh2DemoApplication/src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.Employee;
+
+
+@Service
+public class EmployeeService {
+	private final EmployeeRepository repo;
+	
+	public EmployeeService(EmployeeRepository  repo)
+	{
+		this.repo=repo;
+	}
+	public Employee saveEmployee(Employee employee)
+	{
+		System.out.println(employee.getId()+ "-" +
+	employee.getName() + "-" + employee.getSalary());
+		return repo.save(employee);
+	}
+	public List<Employee> getAllEmployees()
+	{
+		return repo.findAll();
+	}
+	public Employee getEmployeeById(int id)
+	{
+		return repo.findById(id)
+				.orElseThrow(()->
+				new RuntimeException("Employee not found with id:"+id));
+		
+	}
+	public Employee updateEmployee(int id,Employee updatedEmployee)
+	{
+		Employee existingEmployee=repo.findById(id)
+				.orElseThrow(() -> new RuntimeException("employee not found with id"+id));
+		
+		existingEmployee.setName(updatedEmployee.getName());
+		existingEmployee.setSalary(updatedEmployee.getSalary());
+		
+		return repo.save(existingEmployee);
+	}
+	public void deleteEmployee(int id) {
+		Employee existing=repo.findById(id)
+		.orElseThrow(() -> new RuntimeException("employee not found with id"+id));
+		repo.delete(existing);
+	}
+
+}
---
+++ SpringBootJpaHibernateh2DemoApplication/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Employee;
+import com.example.demo.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+	
+	public EmployeeController(EmployeeService employeeService)
+	{
+		this.employeeService=employeeService;
+	}
+	
+	@GetMapping
+	public List<Employee> getemployees()
+	{
+		return employeeService.getAllEmployees();
+	}
+	@GetMapping("/{id}")
+	public Employee GetEmployeebyId(@PathVariable int id)
+	{
+		return employeeService.getEmployeeById(id);
+	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee addEmployee(@RequestBody Employee emp)
+	{
+		return employeeService.saveEmployee(emp);
+	}
+	@PutMapping("/{id}")
+	public Employee updateEmployee(@PathVariable int id,@RequestBody Employee updatedEmp)
+	{
+		return employeeService.updateEmployee(id, updatedEmp);
+		
+	}
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteEmployee(@PathVariable int id)
+	{
+		employeeService.deleteEmployee(id);
+	}
+	
+
+}

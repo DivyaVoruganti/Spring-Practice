0c5b28f84018247a8d88d81fddf68e21a7b93002
DivyaVoruganti <divya.variganti@gmail.com>
2026-02-11 19:13:45 +0530

    thymeleaf practice

---
+++ SpringMvcThymleafDemoApplication/src/main/java/com/example/demo/SpringMvcThymleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymleafDemoApplication.class, args);
+	}
+
+}
